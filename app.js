a767124a6fd79668be1cc143bb8bed382666af6a
Yash-Sajwan24 <yashsajwan12345@gmail.com>
2023-06-01 19:14:57 +0530

    relationship

--- app.js
+++ app.js
@@ -24,27 +24,52 @@ const fruitModel = mongoose.model("Fruit", fruitSchema); //this creates a collec
 
 // fruit.save();
 
-fruitModel
-  .updateOne({ name: "apple" }, { name: "guava" })
-  .then(function () {
-    console.log("successfully updated");
-  })
-  .catch(function (err) {
-    console.log(err);
-  });
+// fruitModel
+//   .updateOne({ name: "apple" }, { name: "guava" })
+//   .then(function () {
+//     console.log("successfully updated");
+//   })
+//   .catch(function (err) {
+//     console.log(err);
+//   });
 
-fruitModel
-  .deleteOne({ name: "grape" })
-  .then(() => console.log("successfully deleted"))
-  .catch((err) => console.log(err));
+// fruitModel
+//   .deleteOne({ name: "grape" })
+//   .then(() => console.log("successfully deleted"))
+//   .catch((err) => console.log(err));
 
-fruitModel
-  .find()
-  .then(function (fruit) {
-    fruit.forEach(function (f) {
-      console.log(f.name);
-    });
-  })
-  .catch(function (err) {
-    console.log(err);
-  });
+// fruitModel
+//   .find()
+//   .then(function (fruit) {
+//     fruit.forEach(function (f) {
+//       console.log(f.name);
+//     });
+//   })
+//   .catch(function (err) {
+//     console.log(err);
+//   });
+
+const pineapple = new fruitModel({
+  name: "pineapple", 
+  rating: 8, 
+  review: "it taste amazing", 
+})
+
+pineapple.save();
+
+const personSchema = new mongoose.Schema({
+  name: String, 
+  age: Number, 
+  favouriteFruit : fruitSchema,
+})
+
+const personModel = mongoose.model("Person", personSchema);
+
+const yash = new personModel({
+  name: "yash", 
+  age: 21, 
+  favouriteFruit: pineapple,
+})
+
+yash.save();
+
