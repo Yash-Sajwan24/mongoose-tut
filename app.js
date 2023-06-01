845cdf635553c80d9f40c14f1bdbd0580dbb03a8
Yash-Sajwan24 <yashsajwan12345@gmail.com>
2023-06-01 16:30:53 +0530

    validation added

--- app.js
+++ app.js
@@ -2,38 +2,28 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://127.0.0.1:27017/fruitsDB');
 
 const fruitSchema = new mongoose.Schema({
-  name: String, 
-  rating : Number, 
+  name: {
+    type: String, 
+    required :[true, "no name specified."],
+  }, 
+  rating : {
+    type: Number, 
+    max: 10, 
+    min: 1
+  }, 
   review: String, 
 });
 
 const fruitModel = mongoose.model("Fruit", fruitSchema);//this creates a collections with name fruits
 
-// const fruit = new fruitModel({
-//   name: "apple", 
-//   rating: 7, 
-//   review : "its an amazing fruit", 
-// });
+const fruit = new fruitModel({
+  name: "apple", 
+  rating: 34, 
+  review : "its an amazing fruit", 
+});
 
-// fruit.save();
+fruit.save();
 
-// const melon  = new fruitModel({
-//   name: "melon", 
-//   rating : 9, 
-//   review: "sweet in taste", 
-// });
-
-// const grape  = new fruitModel({
-//   name: "grape", 
-//   rating : 10, 
-//   review: "amazing in taste", 
-// });
-
-// fruitModel.insertMany([grape,melon]).then(function(){
-//     console.log('successfully inserted');
-// }).catch(function(err){
-//   console.log(err);
-// });
 
 fruitModel.find().then(function(fruit){
     mongoose.connection.close();
